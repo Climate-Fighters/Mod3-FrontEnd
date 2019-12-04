b08092404ad29da5a401847ea6dd73a7dcd37763
github username <github email address>
2019-12-04 17:48:27 -0500

    hard coded graph working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 renderNav();
 renderMainContent();
 renderFooter();
+
+
+function fetchData() {
+  fetch(`http://localhost:3000/countries/`)
+    .then(r => r.json())
+    .then(countriesArray => {
+      countriesArray.forEach((country) => {
+        console.log(country)
+      })
+    })
+    .catch(err => console.log(err))
+}
+
+fetchData()
