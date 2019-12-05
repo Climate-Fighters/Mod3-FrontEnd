f97e0acee89a1449f92c9e1d0c905a8896b84d31
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-12-05 02:30:04 -0500

    fetching complete, data added appropriately, working on implementing them to the graph

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
+
+
 renderNav();
 renderMainContent();
 renderFooter();
 
 
-function fetchData() {
-  fetch(`http://localhost:3000/countries/`)
-    .then(r => r.json())
-    .then(countriesArray => {
-      countriesArray.forEach((country) => {
-        console.log(country)
-      })
-    })
-    .catch(err => console.log(err))
-}
-
-fetchData()
