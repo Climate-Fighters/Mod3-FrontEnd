0cd6bbd734d5e0e0e4eef2d2babfb193c877d6a2
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-12-05 21:03:30 -0500

    Refactored fetch request, toggle functionality working, data added to graph

--- src/main-content-component.js
+++ src/main-content-component.js
@@ -1,170 +1,141 @@
 // Fetching Country Data
 const fetchData = () => {
-  fetch("http://localhost:3000/countries")
-    .then(res => res.json())
-    .then(data => {
-      data.forEach(countryObj => {
-        countryObjData(countryObj)
-        // console.log(countryObj)
-        // if (countryObj.name === "USA") {
-        //   countryObjData(countryObj);
-          
-        // } else if (countryObj.name === "China") {
-        //   countryObjData(countryObj);
-         
-        // } else if (countryObj.name === "India") {
-        //   countryObjData(countryObj);
-        // } else if (countryObj.name === "Brazil") {
-        //   countryObjData(countryObj);
-        // } else {
-        //   countryObjData(countryObj);
-        // }
-        
-      });
-      
-    });
-
+	fetch("http://localhost:3000/countries")
+		.then(res => res.json())
+		.then(data => {
+			data.forEach(countryObj => {
+				countryObjData(countryObj);
+			});
+		});
 };
 fetchData();
 
-
-
-
-
 const countryObjData = countryObj => {
-  let objInfoArr = []
-  
-  // countryInfo[countryObj.name] = {}
+	let objInfoArr = [];
+	let scenarioArr = [];
+	let yieldArr = [];
+	objInfoArr.name = countryObj.name;
 
+	countryObj.country_resources.forEach(countryRes => {
+		let countryProduction = countryRes.production;
+		let countryResourceName = countryRes.resource.name;
 
-  countryObj.country_resources.forEach(countryRes =>{
-    
-    let countryProduction = countryRes.production
-    let countryResourceName = countryRes.resource.name
-    
-     countryRes.resource.resource_scenarios.forEach(scenario =>{
-      
-      let countryScenarioYield = scenario.yield
-      let countryScenarioName =  scenario.scenario.name
-       let countryInfo = Object.assign({}, 
-        {
-          name: countryObj.name,
-          production: countryProduction,
-          resource: countryResourceName,
-          yield: countryScenarioYield,
-          scenario: countryScenarioName,
-        })
-        
-       objInfoArr.push(countryInfo)
-       
-        // Buttons
-      
-    })
-    
-  })
+		countryRes.resource.resource_scenarios.forEach(scenario => {
+			objInfoArr[countryResourceName] = Object.assign(
+				{},
+				{
+					production: countryProduction
+				}
+			);
 
-  
- 
+			scenarioArr.push(scenario.scenario);
+			yieldArr.push(scenario.yield * (countryProduction / 100));
+		});
+	});
 
+	console.log(scenarioArr);
+	renderToggleBtns(objInfoArr, scenarioArr, yieldArr);
 };
-
-
-
 
 const mainContent = document.querySelector(".main-content");
 
+const renderToggleBtns = (objArr, scenarioArr, yieldArr) => {
+	console.log(scenarioArr.name);
+	const countryBtn = document.createElement("button");
+
+	countryBtn.className = `${objArr.name}-btn`;
+	countryBtn.innerHTML = `${objArr.name}`;
+	countryBtn.id = objArr.name;
+
+	mainContent.prepend(countryBtn);
+	renderBarGraph(countryBtn, objArr, scenarioArr, yieldArr);
+};
+const renderBarGraph = (button, objArr, scenarioArr, yieldArr) => {
+  
+	button.addEventListener("click", e => {
+    console.log(objArr)
+		const canvas = document.getElementById("myChart");
+		const ctx = canvas.getContext("2d");
+
+		// stackedBar.clearRect(0, 0, canvas.width, canvas.height)
+		var stackedBar = new Chart(ctx, {
+			type: "bar",
+			data: {
+				labels: [
+					"Current",
+					scenarioArr[0].name,
+					scenarioArr[1].name,
+					scenarioArr[2].name,
+					scenarioArr[3].name
+				],
+				datasets: [
+					{
+						type: "bar",
+						label: "Wheat",
+						backgroundColor: "Tan",
+						hoverBackgroundColor: "Tan",
+						// hoverBorderWidth: 3,
+						// hoverBorderColor: "Black",
+						data: [yieldArr[0], yieldArr[1], yieldArr[2], yieldArr[3]]
+					},
+					{
+						type: "bar",
+						label: "Maize",
+						backgroundColor: "Yellow",
+						hoverBackgroundColor: "Yellow",
+						data: [yieldArr[4], yieldArr[5], yieldArr[6], yieldArr[7]]
+					},
+					{
+						type: "bar",
+						label: "Soybeans",
+						backgroundColor: "Green",
+						hoverBackgroundColor: "Green",
+						data: [yieldArr[8], yieldArr[9], yieldArr[10], yieldArr[11]]
+          },
+        //   {type: "bar",
+        //   label: "amber",
+        //   backgroundColor: "Green",
+        //   hoverBackgroundColor: "Green",
+        //   data: [yieldArr[12], yieldArr[13], yieldArr[14], yieldArr[15]]
+        // }
+				]
+			},
+			options: {
+        responsive: true,
+				title: {
+					display: true,
+					text: `${objArr.name}`
+				},
+				scales: {
+					xAxes: [
+						{
+							stacked: true
+						}
+					],
+					yAxes: [
+						{
+							stacked: true
+						}
+					]
+				}
+			}
+		});
+	});
+};
+
+const renderMainContent = () => {};
 
 // documentation for the chart: https://www.chartjs.org/docs/latest/charts/bar.html#stacked-bar-chart
 
-const renderMainContent = (countryInfoArr) =>{
-  console.log(countryInfoArr)
-  var ctx = document.getElementById('myChart').getContext('2d');
-  
-  var stackedBar = new Chart(ctx, {
-    
-      type: 'bar',
-      data: {
-        labels: [ "Current", "Scenario1", "Scenario2", "Scenario3", "Scenario4"],
-        datasets: [{
-          type: 'bar',
-          label: 'Wheat',
-          backgroundColor: "Tan",
-          hoverBackgroundColor: "Tan",
-          // hoverBorderWidth: 3,
-          // hoverBorderColor: "Black",
-          data: [47, 45, 43, 40, 35],
-        }, {
-          type: 'bar',
-          label: 'Maize',
-          backgroundColor: "Yellow",
-          hoverBackgroundColor: "Yellow",
-          data: [47, 45, 43, 40, 35]
-        }, {
-          type: 'bar',
-          label: 'Soybeans',
-          backgroundColor: "Green",
-          hoverBackgroundColor: "Green",
-          data: [47, 45, 43, 40, 35]
-        }]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'USA'
-        },
-        scales: {
-          xAxes: [{
-            stacked: true
-          }],
-          yAxes: [{
-            stacked: true
-          }]
-        }
-      }
-    });
+// class RenderToggleBtns {
+//   constructor(obj){
+//     this.buttonTag = document.createElement('button')
+//     this.buttonTag.id = obj.id
+//     this.buttonTag.innerText = obj.name
 
-}
-//     type: 'bar',
-//     data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//           label: '# of Votes',
-//           data: [12, 19, 3, 5, 2, 3],
-//           backgroundColor: [
-//               'rgba(255, 99, 132, 0.2)',
-//               'rgba(54, 162, 235, 0.2)',
-//               'rgba(255, 206, 86, 0.2)',
-//               'rgba(75, 192, 192, 0.2)',
-//               'rgba(153, 102, 255, 0.2)',
-//               'rgba(255, 159, 64, 0.2)'
-//           ],
-//           borderColor: [
-//               'rgba(255, 99, 132, 1)',
-//               'rgba(54, 162, 235, 1)',
-//               'rgba(255, 206, 86, 1)',
-//               'rgba(75, 192, 192, 1)',
-//               'rgba(153, 102, 255, 1)',
-//               'rgba(255, 159, 64, 1)'
-//           ],
-//           borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       responsive: false,
-//       scales: {
-//         xAxes: [{
-//           stacked: true
-//         }],
-//         yAxes: [{
-//           ticks: {
-//             beginAtZero: true,
-//             stacked: true
-//           }
-//         }]
-//       }
-//     },
-//   });
-//  console.log('myChart: ', ctx)
+// }
 
-
-
+// usaBtn = new RenderToggleBtns ({
+//   id: 'usa',
+//   name: 'USA'
+// })
